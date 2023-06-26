c439ee64d102413098429d04f10267e4f99b83e6
Süleyman Begic <begic@software.tirol>
2023-06-26 13:55:54 +0200

    Projektdateien hinzufügen.

---
+++ ProductsManager.Data/DataBaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductsManager.Data;
+
+public class DataBaseContext : DbContext
+{
+    public DataBaseContext(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+
+        optionsBuilder.UseSqlServer(@"Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=ProductsManager; Integrated Security=True\");
+    }
+
+    //public DbSet<Product> Products { get; set; }
+}
